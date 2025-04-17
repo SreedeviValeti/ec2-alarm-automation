f378b5baad1fb990ccb8d4abc73a8a1fb78acdbe
Sreedevi Valeti <sridevi.valeti@erpa.com>
2025-04-17 07:01:47 -0400

    alert automation

---
+++ lambda_function/handler.py
@@ -0,0 +1,29 @@
+import boto3
+cloudwatch = boto3.client('cloudwatch')
+
+
+
+def lambda_handler (event, context):
+    instance_id = event['responseElements']['instancesSet']['items'][0]['instanceId']
+
+    response = cloudwatch.put_metric_alarm(
+        AlarmName = f'{instance_id}-cpu-critical-alert',
+        MetricName = "CPUUtilization",
+        Namespace = 'AWS/EC2',
+        Dimensions = [
+            {
+                "Name" : "InstanceId",
+                "Value" : instance_id
+
+            }
+        ],
+        Statistic = "Average",
+        Threshold = 80,
+        ComparisonOperator = "GreaterThanThreshold",
+        Period = 5
+
+    )
+    return {
+        'statusCode': 200,
+        'body': f"Alarm created for {instance_id}"
+    }
---
+++ main.tf
@@ -0,0 +1,91 @@
+resource "aws_cloudwatch_event_bus" "eventbus" {
+    name = "ec2-cloudwatch-alarm-event-bus",
+    description = "This event bus stores all instance launch and terminate events"
+}
+
+resource "aws_cloudwatch_event_rule" "eventruleforec2alarm" {
+    name = "capture-instance-termination-launch-event",
+    description = "This event captures ec2 instance launch and termination events",
+    event_bus_name = aws_cloudwatch_event_bus.eventbus.arn
+    event_pattern = json_encode(
+        {
+            "eventSource" : "ec2.amazonaws.com",
+            "eventName" : "RunInstances",
+            "awsRegion" : "us-east-1",
+           "responseElements": {
+             "instancesSet": {
+                "items": {
+                    "currentState": {
+                        "name": ["running"]
+                    },
+                    "previousState": {
+                        "name": ["pending"]
+                    }
+                }
+            }
+        }
+    }
+    )
+
+}
+
+resource "aws_cloudwatch_event_target" "alerttarget" {
+    arn = "",
+    rule = aws_cloudwatch_event_rule.eventruleforec2alarm.id
+}
+
+resource "aws_iam_policy" "iam_policy_for_alert_lambda" {
+    name = "iam-alert-policy",
+    path = "/",
+    policy = jsonencode({
+        "Version" : "2012-10-17",
+        "Statement" : [
+            {
+                "Action" : "cloudwatch:*",
+                "Effect" : "Allow",
+                "Resource" : "*"
+            }
+        ]
+
+    })
+}
+
+resource "aws_iam_role" "lambda_assume_role" {
+    name = "cloudwatch-alert-automation-lambda",
+    assume_role_policy = jsonencode({
+        "Version" : "2012-10-17",
+        "Statement" : [
+            {
+                "Action" : "sts:AssumeRole",
+                "Effect" : "Allow",
+                "Principal" : {
+                    "Service" = "lambda.amazonaws.com"
+                }
+            }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attatchment" "lambda_permission_policy_attachement"{
+    role = aws_iam_role.lambda_assume_role.name
+    policy_arn = aws_iam_policy.iam_policy_for_alert_lambda.arn
+}
+
+
+data "archive_file" "code" {
+    type = "zip",
+    source_dir = "lambda_function",
+    output_path = lambda_function.zip
+}
+
+resource "aws_lambda_function" "alert_lambda" {
+    function_name = "alert-create-automate-lambda",
+    role = aws_iam_role.lambda_assume_role.arn,
+    runtime = "python3.9",
+    source_code_hash = data.archive_file.code.output_path_base64sha256,
+    timeout = 600,
+    package_type = "Zip",
+    handler = "handler.lambda_handler",
+    filename = data.archive_file.code.output_path
+}
+
